1daa85acc573badcbb2d82813d818cfa338acf4e
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-10-24 17:17:01 -0300

    feat: adicionando ícones SVG para relatórios e atualizando o componente Card para incluir novos links de relatórios.

--- src/app/relatorios/page.tsx
+++ src/app/relatorios/page.tsx
@@ -1,17 +1,48 @@
 "use client"
 import Cabecalho from "@/components/cabecalho"
 import Card from "@/components/cards-relatorios"
+import { useRef, useState, useEffect } from "react"
 export default function RelarotiosPage() {
+
+    // const cardRef = useRef<HTMLDivElement>(null)
+  
+    // const [isCompact, setIsCompact] = useState(false)
+  
+    // useEffect(() => {
+    //   const observer = new ResizeObserver(([entry]) => {
+    //     setIsCompact(entry.contentRect.width < 300)
+    //   });
+    //   if (cardRef.current) observer.observe(cardRef.current);
+    //   return () => observer.disconnect()
+    // }, [])
+
   return (
-    <div className="w-full bg-[#F9FAFB] h-full">
+    <div className="w-full h-full bg-[#F9FAFB] flex flex-col justify-between pb-[20px] items-center">
       <Cabecalho pagina="Relatórios" />
-      <div className="p-6 pt-0">
+      <div className="flex justify-center w-full px-8">
+         <div className="flex flex-wrap justify-center gap-[30px] w-full max-w-[1500px]">
         <Card
         title="Componentes"
         descricao="Visualize relatórios completos sobre componentes e status de estoque."
         imagem="./relatorios-componentes.svg"
+        bg_imagem="bg-[#DBEAFE]"
+        />
+        <Card
+        title="Movimentações"
+        descricao="Acompanhe entradas e saídas de componentes do estoque."
+        imagem="./relatorios-movimentacoes.svg"
+        bg_imagem="bg-[#E5DDFE]"
+        />
+        <Card
+        title="Orçamentos"
+        descricao="Acesse o histórico completo de orçamentos."
+        imagem="./relatorios-orcamentos.svg"
+        bg_imagem="bg-[#DCFCE7]"
         />
       </div>
+      </div>
+      <div></div>
+      <div></div>
     </div>
   )
 }
--- src/components/cards-relatorios.tsx
+++ src/components/cards-relatorios.tsx
@@ -1,26 +1,46 @@
+import { useEffect, useRef, useState } from "react"
 
 interface ICard {
-  title:string,
-  descricao:string,
-  imagem:string
+  title: string,
+  descricao: string,
+  imagem: string
+  bg_imagem:string
 }
 
-export default function Card({title, descricao, imagem}: ICard){
-  return(
-    <div className="bg-[#FFFFFF] border-[#4F668C] border-[0.5px] rounded-[10px] max-w-[475px]">
-      <div className="flex flex-row">
-        <div>
-          <div className="flex items-center justify-center w-[100px] h-[100px] bg-[#DBEAFE] rounded-[50%]">
+export default function Card({ title, descricao, imagem, bg_imagem }: ICard) {
+
+  // obter card
+  const cardRef = useRef<HTMLDivElement>(null)
+
+  const [isCompact, setIsCompact] = useState(false)
+
+  useEffect(() => {
+    const observer = new ResizeObserver(([entry]) => {
+      setIsCompact(entry.contentRect.width < 300)
+    });
+    if (cardRef.current) observer.observe(cardRef.current);
+    return () => observer.disconnect()
+  }, [])
+
+  return (
+    <div className="bg-[#FFFFFF] rounded-[10px] max-w-[475px] cursor-pointer hover drop-shadow-[2px_2px_4px_rgba(0,0,0,0.20)] overflow-hidden transition-transform duration-300 hover:scale-105">
+      <div ref={cardRef} className={isCompact ? "flex flex-col gap-[30px] p-[35px] items-center text-center" : "flex flex-row gap-[30px] p-[35px]"}>
+        <div className={isCompact ? "" : "pt-[10px] flex-shrink-0"}>
+          <div className={"flex items-center justify-center w-[100px] h-[100px] rounded-[50%] " + bg_imagem}>
             <img src={imagem} alt="" />
           </div>
         </div>
-        <div>
-          <h4>{title}</h4>
-          <span>{descricao}</span>
+        <div className="flex-1 min-w-0">
+          <h2 className="font-semibold text-[26px] mb-[5px] overflow-hidden text-ellipsis whitespace-nowrap">{title}</h2>
+          <span className="text-[#6B7280] font-semibold text-[16px] block overflow-hidden text-ellipsis line-clamp-2">{descricao}</span>
         </div>
       </div>
-      <div>
-        <a href="/componentes">Acessar </a>
+      <hr className=" border-[#4f668c71]" />
+      <div className="text-center p-[16px] text-[#5676A1]overflow-hidden">
+        <a className="inline-flex font-medium items-center group transition-colors duration-300 hover:text-[#3d5576]" href="/relatorios/componentes">
+          Acessar
+          <img className="ml-[8px] w-[6px] transition-transform duration-300 group-hover:translate-x-1" src="/acessar.svg" alt="" />
+        </a>
       </div>
     </div>
   )
