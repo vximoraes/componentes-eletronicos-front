f9a20dff1a6a88fb15f322aa8cd15ac2c2abe5b1
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-11-27 19:30:31 -0400

    feat: adiciona configuração e comandos para testes automatizados com Cypress, incluindo variáveis de ambiente e novos helpers

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,15 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.baseUrl = config.env.FRONTEND_URL || 'http://localhost:3000';
+      return config;
     },
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+    video: false,
+    screenshotOnRunFailure: true,
   },
 });
